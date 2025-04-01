f5caffc80af318ca3ecf98e144a640f01c656f6f
Philip Dodds <5761+pdodds@users.noreply.github.com>
2025-04-01 09:58:15 -0400

    Refactor infer function in model.py to improve parameter naming for clarity; update logging statements to reflect changes in project and document family references.

--- {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/model.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/model.py
@@ -7,16 +7,16 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def infer(document: Document, project_endpoint: ProjectEndpoint, document_family_endpoint: DocumentFamilyEndpoint, client: KodexaClient, pipeline_context: PipelineContext, assistant: Assistant):
+def infer(document: Document, project: ProjectEndpoint, document_family: DocumentFamilyEndpoint, client: KodexaClient, pipeline_context: PipelineContext, assistant: Assistant):
 
     logger.info(f"Infer called with document: {document.uuid}")
 
-    logger.info(f"Project: {project_endpoint.name}")
-    logger.info(f"Document Family: {document_family_endpoint.path}")
+    logger.info(f"Project: {project.name}")
+    logger.info(f"Document Family: {document_family.path}")
 
     # Lets see if we have extracted any data
     logger.info("Extracted Data:")
-    logger.info(document_family_endpoint.get_json(project_id=project_endpoint.project_id, include_exceptions=True, inline_audits=True))
+    logger.info(document_family.get_json(project_id=project.project_id, include_exceptions=True, inline_audits=True))
 
     # Lets add a label to the document
     document.add_label("my_first_model")
