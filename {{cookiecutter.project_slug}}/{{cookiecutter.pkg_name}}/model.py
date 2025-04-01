48a53789baa079e359e8998b307310bf17aec1e0
Philip Dodds <5761+pdodds@users.noreply.github.com>
2025-04-01 09:54:10 -0400

    Enhance infer function in model.py to accept additional parameters for KodexaClient, PipelineContext, and Assistant; add logging for native document source, client platform, and pipeline context.

--- {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/model.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/model.py
@@ -1,13 +1,14 @@
 """Main module."""
 
-from kodexa import Document
+from kodexa import Document, get_source, PipelineContext, KodexaClient, Assistant
 from kodexa.platform.client import ProjectEndpoint, DocumentFamilyEndpoint
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def infer(document: Document, project_endpoint: ProjectEndpoint, document_family_endpoint: DocumentFamilyEndpoint):
+def infer(document: Document, project_endpoint: ProjectEndpoint, document_family_endpoint: DocumentFamilyEndpoint, client: KodexaClient, pipeline_context: PipelineContext, assistant: Assistant):
+
     logger.info(f"Infer called with document: {document.uuid}")
 
     logger.info(f"Project: {project_endpoint.name}")
@@ -19,5 +20,20 @@ def infer(document: Document, project_endpoint: ProjectEndpoint, document_family
 
     # Lets add a label to the document
     document.add_label("my_first_model")
+
+    # Lets download the native document (the source PDF or file)
+    with get_source(document) as native_source:
+        # Load document directly from bytes
+        source_bytes: bytes = native_source.read()
+        logger.info(f"Source Bytes: {len(source_bytes)}")
+
+    # We also have a client configured
+    logger.info(f"Client: {client.get_platform()}")
+
+    # We can also get the pipeline context
+    logger.info(f"Pipeline Context: {pipeline_context}")
+
+    # We can also get the assistant
+    logger.info(f"Assistant: {assistant}")
 
     return document
